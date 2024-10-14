a95704813e1ca5c9b690786016ac65aaf949a2e6
psiconervio <psiconervio@gmail.com>
2024-10-14 02:46:28 -0300

    asdasd

--- shcdn-typescrytp-solo/app/settings/page.tsx
+++ shcdn-typescrytp-solo/app/settings/page.tsx
@@ -23,8 +23,8 @@ import {
 import { Input } from "@/components/ui/input"
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
 
-export const description =
-  "A settings page. The settings page has a sidebar navigation and a main content area. The main content area has a form to update the store name and a form to update the plugins directory. The sidebar navigation has links to general, security, integrations, support, organizations, and advanced settings."
+// export const description =
+//   "A settings page. The settings page has a sidebar navigation and a main content area. The main content area has a form to update the store name and a form to update the plugins directory. The sidebar navigation has links to general, security, integrations, support, organizations, and advanced settings."
 
 export function Dashboard() {
   return (
