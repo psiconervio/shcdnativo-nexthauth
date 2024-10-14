7437a39e982e7bf38bbd1492df24364cce9a1a23
psiconervio <psiconervio@gmail.com>
2024-10-14 13:05:52 -0300

    asdsd

--- shcdn-typescrytp-solo/app/api/auth/register/route.ts
+++ shcdn-typescrytp-solo/app/api/auth/register/route.ts
@@ -1,65 +1,72 @@
-import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
-import db from "@/lib/db";
+import NextAuth from "next-auth";
+import { authOptions } from '@/lib/authOptions'; // Asegúrate de la ruta correcta
 
-export async function POST(request) {
-  try {
-    const data = await request.json();
+const handler = NextAuth(authOptions);
 
-    const userFound = await db.user.findUnique({
-      where: {
-        email: data.email,
-      },
-    });
+export { handler as GET, handler as POST };
 
-    if (userFound) {
-      return NextResponse.json(
-        {
-          message: "Email already exists",
-        },
-        {
-          status: 400,
-        }
-      );
-    }
+// import { NextResponse } from "next/server";
+// import bcrypt from "bcrypt";
+// import db from "@/lib/db";
 
-    const usernameFound = await db.user.findUnique({
-      where: {
-        username: data.username,
-      },
-    });
+// export async function POST(request) {
+//   try {
+//     const data = await request.json();
 
-    if (usernameFound) {
-      return NextResponse.json(
-        {
-          message: "username already exists",
-        },
-        {
-          status: 400,
-        }
-      );
-    }
+//     const userFound = await db.user.findUnique({
+//       where: {
+//         email: data.email,
+//       },
+//     });
 
-    const hashedPassword = await bcrypt.hash(data.password, 10);
-    const newUser = await db.user.create({
-      data: {
-        username: data.username,
-        email: data.email,
-        password: hashedPassword,
-      },
-    });
+//     if (userFound) {
+//       return NextResponse.json(
+//         {
+//           message: "Email already exists",
+//         },
+//         {
+//           status: 400,
+//         }
+//       );
+//     }
 
-    const { password: _, ...user } = newUser;
+//     const usernameFound = await db.user.findUnique({
+//       where: {
+//         username: data.username,
+//       },
+//     });
 
-    return NextResponse.json(user);
-  } catch (error) {
-    return NextResponse.json(
-      {
-        message: error.message,
-      },
-      {
-        status: 500,
-      }
-    );
-  }
-}
+//     if (usernameFound) {
+//       return NextResponse.json(
+//         {
+//           message: "username already exists",
+//         },
+//         {
+//           status: 400,
+//         }
+//       );
+//     }
+
+//     const hashedPassword = await bcrypt.hash(data.password, 10);
+//     const newUser = await db.user.create({
+//       data: {
+//         username: data.username,
+//         email: data.email,
+//         password: hashedPassword,
+//       },
+//     });
+
+//     const { password: _, ...user } = newUser;
+
+//     return NextResponse.json(user);
+//   } catch (error) {
+//     return NextResponse.json(
+//       {
+//         message: error.message,
+//       },
+//       {
+//         status: 500,
+//       }
+//     );
+//   }
+// }
