f993b9883fc83bde0314ed0bc06547b803eee411
psiconervio <psiconervio@gmail.com>
2024-10-14 02:34:31 -0300

    dsasad

--- shcdn-typescrytp-solo/app/dashboard/products/editproducts/page.tsx
+++ shcdn-typescrytp-solo/app/dashboard/products/editproducts/page.tsx
@@ -72,8 +72,8 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip"
 
-export const description =
-  "A product edit page. The product edit page has a form to edit the product details, stock, product category, product status, and product images. The product edit page has a sidebar navigation and a main content area. The main content area has a form to edit the product details, stock, product category, product status, and product images. The sidebar navigation has links to product details, stock, product category, product status, and product images."
+// export const description =
+//   "A product edit page. The product edit page has a form to edit the product details, stock, product category, product status, and product images. The product edit page has a sidebar navigation and a main content area. The main content area has a form to edit the product details, stock, product category, product status, and product images. The sidebar navigation has links to product details, stock, product category, product status, and product images."
 
 export default function Dashboard() {
   return (
