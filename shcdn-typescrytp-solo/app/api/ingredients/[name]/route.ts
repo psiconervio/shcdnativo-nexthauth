d6d685c9b31758517a05aa15b5a1cf5ab3600bbb
psiconervio <psiconervio@gmail.com>
2024-10-22 12:27:55 -0300

    asdad

--- shcdn-typescrytp-solo/app/api/ingredients/[name]/route.ts
+++ shcdn-typescrytp-solo/app/api/ingredients/[name]/route.ts
@@ -3,18 +3,41 @@ import { NextResponse } from 'next/server';
 import  prisma  from '@/lib/db';
 
 export async function GET(request, { params }) {
+  const { name } = params;
+
   try {
-    const ingredient = await prisma.ingredient.findUnique({
-      where: { id: parseInt(params.id) },
+    const ingredient = await prisma.ingredient.findFirst({
+      where: {
+        name: {
+          contains: name,
+          mode: 'insensitive',
+        },
+      },
     });
+
     if (!ingredient) {
       return NextResponse.json({ error: 'Ingredient not found' }, { status: 404 });
     }
+
     return NextResponse.json(ingredient);
   } catch (error) {
     return NextResponse.json({ error: 'Error fetching ingredient' }, { status: 500 });
   }
 }
+//BUSCA POR ID, CAMBIAR EL NOMBRE DE CARPETA [NAME] A [ID]
+// export async function GET(request, { params }) {
+//   try {
+//     const ingredient = await prisma.ingredient.findUnique({
+//       where: { id: parseInt(params.id) },
+//     });
+//     if (!ingredient) {
+//       return NextResponse.json({ error: 'Ingredient not found' }, { status: 404 });
+//     }
+//     return NextResponse.json(ingredient);
+//   } catch (error) {
+//     return NextResponse.json({ error: 'Error fetching ingredient' }, { status: 500 });
+//   }
+// }
 
 export async function PUT(request, { params }) {
   try {
