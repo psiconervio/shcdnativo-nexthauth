55ea2d63084193a0e64ca060f5de9e99eeabe890
psiconervio <psiconervio@gmail.com>
2024-11-12 23:01:46 -0300

    asdasd

--- shcdn-typescrytp-solo/app/api/inventory/adjust/route.ts
+++ shcdn-typescrytp-solo/app/api/inventory/adjust/route.ts
@@ -1,6 +1,6 @@
 // src/app/api/inventory/adjust/route.js
 import { NextResponse } from 'next/server';
-import { prisma } from '@/lib/prisma';
+import  prisma  from '@/lib/db';
 
 export async function POST(request) {
   try {
