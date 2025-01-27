7a35382d959d0d0bfff0053ecd79991b537d5d76
psiconervio <psiconervio@gmail.com>
2025-01-27 07:30:18 -0300

    asdasd

--- shcdn-typescrytp-solo/app/dashboard/page.tsx
+++ shcdn-typescrytp-solo/app/dashboard/page.tsx
@@ -48,71 +48,6 @@ export default function Dashboard() {
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
             <ItemsDashboard />
-            {/* <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Productos Disponibles
-                </CardTitle>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-package"><path d="M11 21.73a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73z"/><path d="M12 22V12"/><polyline points="3.29 7 12 12 20.71 7"/><path d="m7.5 4.27 9 5.15"/></svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">$45,231.89</div>
-                <p className="text-xs text-muted-foreground">
-                  +20.1% from last month
-                </p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Productos Defectuosos
-                </CardTitle>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-package-x"><path d="M21 10V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l2-1.14"/><path d="m7.5 4.27 9 5.15"/><polyline points="3.29 7 12 12 20.71 7"/><line x1="12" x2="12" y1="22" y2="12"/><path d="m17 13 5 5m-5 0 5-5"/></svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+2350</div>
-                <p className="text-xs text-muted-foreground">
-                  +180.1% from last month
-                </p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">Ventas</CardTitle>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-badge-dollar-sign"><path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z"/><path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8"/><path d="M12 18V6"/></svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+12,234</div>
-                <p className="text-xs text-muted-foreground">
-                  +19% from last month
-                </p>
-              </CardContent>
-            </Card>
-            <Card>
-              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                <CardTitle className="text-sm font-medium">
-                  Active Now
-                </CardTitle>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="h-4 w-4 text-muted-foreground"
-                >
-                  <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                </svg>
-              </CardHeader>
-              <CardContent>
-                <div className="text-2xl font-bold">+573</div>
-                <p className="text-xs text-muted-foreground">
-                  +201 since last hour
-                </p>
-              </CardContent>
-            </Card> */}
           </div>
 
           <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7 mb-8">
@@ -120,9 +55,9 @@ export default function Dashboard() {
             <Resumenstock />
           </div>
           <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-7 mb-8">
-            <RecentSales />
+          <RecentSaleslegacy />
             <Produccionstock/>
-            <RecentSaleslegacy />
+            <RecentSales />
           </div>
         </div>
       </main>
--- shcdn-typescrytp-solo/components/ventas-recientes-legacy.tsx
+++ shcdn-typescrytp-solo/components/ventas-recientes-legacy.tsx
@@ -1,70 +1,99 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "./ui/table";
-import useSWR from 'swr';
-
-
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import useSWR from "swr";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 const recentSales = [
-    {
-      id: 1,
-      name: "Olivia Martin",
-      email: "olivia.martin@email.com",
-      amount: "+$1,999.00",
-    },
-    { id: 2, name: "Jackson Lee", email: "jackson.lee@email.com", amount: "+$39.00" },
-    {
-      id: 3,
-      name: "Isabella Nguyen",
-      email: "isabella.nguyen@email.com",
-      amount: "+$299.00",
-    },
-    { id: 4, name: "William Kim", email: "will@email.com", amount: "+$99.00" },
-    { id: 5, name: "Sofia Davis", email: "sofia.davis@email.com", amount: "+$39.00" },
-  ];
-  export function RecentSaleslegacy() {
-    const { data: ventas, error: stockError } = useSWR('/api/sales', fetcher);
-    console.log(ventas)
-    if (stockError) return <div>Error al cargar los datos</div>;
-    if (!ventas) return <div>Cargando...</div>;
+  {
+    id: 1,
+    name: "Olivia Martin",
+    email: "olivia.martin@email.com",
+    amount: "+$1,999.00",
+  },
+  {
+    id: 2,
+    name: "Jackson Lee",
+    email: "jackson.lee@email.com",
+    amount: "+$39.00",
+  },
+  {
+    id: 3,
+    name: "Isabella Nguyen",
+    email: "isabella.nguyen@email.com",
+    amount: "+$299.00",
+  },
+  { id: 4, name: "William Kim", email: "will@email.com", amount: "+$99.00" },
+  {
+    id: 5,
+    name: "Sofia Davis",
+    email: "sofia.davis@email.com",
+    amount: "+$39.00",
+  },
+];
+export function RecentSaleslegacy() {
+  const { data: ventas, error: stockError } = useSWR("/api/sales", fetcher);
+  console.log(ventas);
+  if (stockError) return <div>Error al cargar los datos</div>;
+  if (!ventas) return <div>Cargando...</div>;
 
-    return (
-        <Card className="col-span-full lg:col-span-4">
-        <CardHeader>
-          <CardTitle>Ventas Recientes</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHead>Cliente</TableHead>
-                <TableHead>Productos</TableHead>
-                <TableHead>Saldo</TableHead>
-                <TableHead>Fecha</TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {recentSales.map((item) => (
-                <TableRow key={item.name}>
-                  <TableCell className="flex items-center">
-                    <Avatar>
-                      <AvatarFallback>{item.name[0]}</AvatarFallback>
+  return (
+    <Card className="col-span-full lg:col-span-4">
+      <CardHeader>
+        <CardTitle>Ventas Recientes</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Cliente</TableHead>
+              <TableHead>Productos</TableHead>
+              <TableHead>Saldo</TableHead>
+              <TableHead>Cantidad</TableHead>
+              <TableHead>Fecha</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {ventas.map((venta) => (
+              <TableRow key={venta.id}>
+                <TableCell className="flex items-center">
+                  <Avatar>
+                    {/* <AvatarFallback>{item.name[0]}</AvatarFallback>
                       <AvatarImage
                         src={`https://randomuser.me/api/portraits/men/${item.id}.jpg`}
                         alt={item.name}
-                      />
-                    </Avatar>
-                    <span className="ml-2">{item.name}</span>
-                  </TableCell>
-                  <TableCell>{item.email}</TableCell>
-                  <TableCell>{item.amount}</TableCell>
-                  <TableCell>{item.amount}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </CardContent>
-      </Card>
-    )
+                      /> */}
+                  </Avatar>
+                  <span className="ml-2">{venta.client?.name}</span>
+                </TableCell>
+                <TableCell>
+                  {venta.products.map((product) => (
+                    <div key={product.id}>{product.product?.name}</div>
+                  ))}
+                </TableCell>
+                <TableCell>
+                  {venta.products.map((product) => (
+                    <div key={product.id}>${product.totalPrice.toFixed(2)}</div>
+                  ))}
+                </TableCell>
+                <TableCell>
+                  {venta.products.map((product) => (
+                    <div key={product.id}>{product.quantity}</div>
+                  ))}
+                </TableCell>
+                <TableCell>{venta.date}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
 }
