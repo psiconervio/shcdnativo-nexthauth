3a73de91075b52631f1f38ce2309e7730998fff2
psiconervio <psiconervio@gmail.com>
2025-01-27 06:17:23 -0300

    ASDASD

--- shcdn-typescrytp-solo/app/ventas/page.tsx
+++ shcdn-typescrytp-solo/app/ventas/page.tsx
@@ -67,7 +67,6 @@ export default function Dashboard() {
               <NewSaleModal />
             </div>
           </div>
-
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
             <ItemsDashboard />
           </div>
