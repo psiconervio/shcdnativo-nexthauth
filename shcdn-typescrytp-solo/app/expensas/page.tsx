e2cb391e8e86adb02b58e0c74ddfd370716ae186
psiconervio <psiconervio@gmail.com>
2025-01-31 02:02:29 -0300

    asdasd

--- shcdn-typescrytp-solo/app/expensas/page.tsx
+++ shcdn-typescrytp-solo/app/expensas/page.tsx
@@ -4,9 +4,10 @@ import { TablaExpensas } from "@/components/tabla-expensas";
 export default function Expensas( ){
 
     return (
-        <>
-        <h1>Expensas</h1>
-        <TablaExpensas />
-        </>
+        <div className="min-h-screen">
+      <main className="container mx-auto p-2 sm:p-6">
+        <TablaExpensas/>
+      </main>
+        </div>
     )
 }
